777bb9db49f6c1bd7e35692490f8efefb34a0983
AimeeYang <1014279248@qq.com>
2016-01-26 11:27:13 +0800

    helloworld

---
+++ HelloWorld/src/main/scala/helloworlddemo/HelloWorld.scala
@@ -0,0 +1,15 @@
+package helloworlddemo
+
+/**
+  * Created by ayang on 2016/1/25.
+  */
+class HelloWorld {
+
+}
+
+object HelloWorld{
+  def main(args:Array[String]){
+    println("Hello world.")
+    //args.foreach(x => println(x))
+  }
+}
